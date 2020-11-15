77bec23e51cb66d2223ca494402e5cf2898232e8
Yann <y.saint-michel@outlook.com>
2020-11-15 12:22:30 +0100

    ajout du backend

---
+++ backend/server.js
@@ -0,0 +1,71 @@
+
+const http = require('http');
+//on importe l'application app.js :
+const app = require('./app');
+/*
+serveur de base :
+const server = http.createServer((req, res) => {
+    res.end('voila la reponse du serveur avec nodemon! YES');
+});
+*/
+/*on passe notre application au server et sur quelle port elle va tourner :
+app.set("port", process.env.PORT || 3000);
+const server = http.createServer(app);
+*/
+//server.listen(process.env.PORT || 3000);
+/*
+installation de nodemon avec 'npm install -g nodemon'
+ce qui permet d'éviter de rappeler 'node server' plusieurs fois dans le terminal
+on appelle une seule fois 'nodemon server'
+*/
+
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      return val;
+    }
+    if (port >= 0) {
+      return port;
+    }
+    return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+  
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+    case 'EACCES':
+        console.error(bind + ' requires elevated privileges.');
+        process.exit(1);
+        break;
+    case 'EADDRINUSE':
+        console.error(bind + ' is already in use.');
+        process.exit(1);
+        break;
+    default:
+        throw error;
+    }
+};
+  
+const server = http.createServer(app);
+  
+server.on('error', errorHandler);
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
+});
+  
+  server.listen(port);
+/* Explication :
+la fonction normalizePort renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne ;
+la fonction errorHandler  recherche les différentes erreurs et les gère de manière appropriée. Elle est ensuite enregistrée dans le serveur ;
+un écouteur d'évènements est également enregistré, consignant le port ou le canal nommé sur lequel le serveur s'exécute dans la console.
+Notre serveur de développement Node est à présent opérationnel. Vous pouvez ainsi ajouter les fonctionnalités appropriées à l'application Express.
+*/
